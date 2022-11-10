f65e3e390c0ca5cfe5a63f56f39acd6486d15099
olga-demina <olga.demina1410@gmail.com>
2022-11-10 10:19:08 +0100

    Added comic card with props

---
+++ src/components/ComicCard.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+    name: "ComicCard",
+    props: {
+        // imgSrc: String,
+        // title: String
+        comicObj: Object
+    }
+}
+</script>
+
+<template>
+    <div class="card">
+        <img class="card-img" :src="comicObj.thumb" alt="">
+        <h3 class="card-serie">{{ comicObj.series }}</h3>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use "../styles/partials/variables" as *;
+
+.card {
+    width: calc(100% / 6 - 10px);
+    margin-bottom: 5px;
+
+    &-serie {
+        color: $dark_section_text_color;
+    }
+}
+</style>
